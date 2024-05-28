9a8c013a283f6641710d4f058acdb6edf3fb8f62
akshupdf <57107903+akshupdf@users.noreply.github.com>
2024-05-28 23:59:23 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodeMailer = require('nodemailer');
+require('dotenv').config({ path: './config/config.env' })
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.post('/feedback',async (req, res)=>{
+    
+    const { name, email, message } = req.body;
+   
+    try{
+    const transporter = nodeMailer.createTransport({
+        host: "smtp.gmail.com",
+        port:587,
+        service: process.env.SMPT_SERVICE ,
+        auth:{
+            user: process.env.SMPT_MAIL,
+            pass:process.env.SMPT_PASSWORD
+        }
+    })
+    
+    const mailOptions = {
+        from: process.env.SMPT_MAIL,
+        to: "sspl.website.leads@gmail.com",
+        subject: 'Feedback Received',
+        text : `Name: ${name}\nEmail: ${email}\nMessage: ${message}`
+    }
+  
+
+    await transporter.sendMail(mailOptions)
+
+    res.status(200).json({
+        success:true,
+        message:`Feedback Received successfully`
+    })
+
+
+}catch(err){
+    console.error('Error sending email:', err);
+    res.status(500).json({ error: 'Server error' });
+} })
+
+
+
+app.listen(PORT, () => {
+    console.log('Server listening on port 4000');
+  })
